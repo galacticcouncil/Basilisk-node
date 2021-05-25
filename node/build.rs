f92a009d1d96ab66e1ca1e25b3b01c1be35fbaee
Martin Hloska <martin.hloska@gmail.com>
2021-05-25 22:36:00 +0200

    refactor: use build script utils from hydradx

--- node/build.rs
+++ node/build.rs
@@ -1,6 +1,6 @@
-use basilisk_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
+use hydra_dx_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 fn main() {
-	generate_cargo_keys().expect("Failed to generate version metadata");
+	generate_cargo_keys("basilisk-runtime").expect("Failed to generate version metadata");
 	rerun_if_git_head_changed();
 }
--- utils/build-script-utils/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Crate with utility functions for `build.rs` scripts.
-
-mod git;
-mod version;
-
-pub use git::*;
-pub use version::*;
