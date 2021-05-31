c0b67da3b218df69bc9bf1976d1d46db534ee40d
Jakub Pánik <jakub.panik@gmail.com>
2021-05-31 09:18:36 +0100

    bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -106,8 +106,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("basilisk"),
 	impl_name: create_runtime_str!("basilisk"),
 	authoring_version: 1,
-	spec_version: 3,
-	impl_version: 1,
+	spec_version: 4,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
 };
