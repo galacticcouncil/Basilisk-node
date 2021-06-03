ec4b0006b3dc656a8580172265ba80ca00616fcd
martinfridrich <mrtnfridrich@gmail.com>
2021-06-03 14:12:04 +0200

    removed EPOCH_DURATION_IN_BLOCKS

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -121,8 +121,6 @@ pub const MILLISECS_PER_BLOCK: u64 = 6000;
 
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 
-pub const EPOCH_DURATION_IN_BLOCKS: u32 = 10 * MINUTES;
-
 // Time is measured by number of blocks.
 pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
 pub const HOURS: BlockNumber = MINUTES * 60;
