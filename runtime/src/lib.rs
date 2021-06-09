316284f641e5dc933a0800ac5291f4bf41b9e6a4
green-jay <greenee@email.cz>
2021-06-09 14:16:08 +0200

    bump

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -110,7 +110,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("basilisk"),
 	impl_name: create_runtime_str!("basilisk"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
