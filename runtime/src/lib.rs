38937f4147c938f8c330342978bc45fa2d17a784
Martin <martin.hloska@gmail.com>
2021-08-27 09:39:21 +0200

    increase democrary max votes and proposals

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -520,8 +520,8 @@ parameter_types! {
 	pub const CooloffPeriod: BlockNumber = 7 * DAYS;
 	pub const PreimageByteDeposit: Balance = CENTS;
 	pub const InstantAllowed: bool = true;
-	pub const MaxVotes: u32 = 30;
-	pub const MaxProposals: u32 = 30;
+	pub const MaxVotes: u32 = 100;
+	pub const MaxProposals: u32 = 100;
 }
 
 type EnsureMajorityCouncilOrRoot = frame_system::EnsureOneOf<
