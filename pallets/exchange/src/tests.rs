3b8a6af1e29102c8985f2cc0d9e96eb7819e6d21
Martin <martin.hloska@gmail.com>
2021-10-15 14:01:15 +0200

    test: add additional exchange tests

--- pallets/exchange/src/tests.rs
+++ pallets/exchange/src/tests.rs
@@ -4389,3 +4389,219 @@ fn trade_limit_test() {
 			assert_eq!(Exchange::get_intentions_count((asset_a, asset_b)), 0);
 		});
 }
+
+// TODO: register_intention, execute_amm_transfer, verify_intention, resolve_single_intention
+
+#[test]
+fn register_intention_should_work() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Exchange::register_intention(
+			&ALICE,
+			IntentionType::SELL,
+			AssetPair {
+				asset_in: HDX,
+				asset_out: DOT
+			},
+			1,
+			1,
+			1,
+			false
+		));
+		assert_ok!(Exchange::register_intention(
+			&ALICE,
+			IntentionType::BUY,
+			AssetPair {
+				asset_in: HDX,
+				asset_out: DOT
+			},
+			1,
+			1,
+			1,
+			false
+		));
+
+		assert_eq!(Exchange::get_intentions_count((HDX, DOT)), 2);
+		assert_eq!(Exchange::get_intentions((HDX, DOT)).len(), 2);
+	});
+}
+
+// #[test]
+// fn register_intention_should_return_error_on_overflow() {
+// 	new_test_ext().execute_with(|| {
+// 		ExchangeAssetsIntentionCount::<Test>::insert((HDX, DOT), u32::MAX);
+// 		assert_eq!(Exchange::get_intentions_count((HDX, DOT)), u32::MAX);
+
+// 		assert!(Exchange::register_intention(&ALICE, IntentionType::SELL, AssetPair { asset_in: HDX, asset_out: DOT }, 1, 1, 1, false).is_err());
+// 	});
+// }
+
+#[test]
+fn execute_amm_transfer_should_work() {
+	new_test_ext().execute_with(|| {
+		let asset_a = HDX;
+		let asset_b = DOT;
+		let pool_amount = 1_000_000_000_000_000;
+		let initial_price = Price::from_float(0.072);
+		initialize_pool(asset_b, asset_a, ALICE, pool_amount, initial_price);
+
+		let pair_account = XYKPallet::get_pair_id(AssetPair {
+			asset_in: asset_a,
+			asset_out: asset_b,
+		});
+
+		let alice_buy_intention_id = generate_intention_id(&ALICE, 0);
+		assert_ok!(Exchange::execute_amm_transfer(
+			IntentionType::BUY,
+			alice_buy_intention_id,
+			&AMMTransfer {
+				origin: ALICE,
+				assets: AssetPair {
+					asset_in: HDX,
+					asset_out: DOT
+				},
+				amount: 1_000_000_000_000,
+				amount_out: 1_000_000_000,
+				discount: false,
+				discount_amount: 0,
+				fee: (1000, 1_000_000),
+			}
+		));
+
+		let alice_sell_intention_id = generate_intention_id(&ALICE, 1);
+		assert_ok!(Exchange::execute_amm_transfer(
+			IntentionType::SELL,
+			alice_sell_intention_id,
+			&AMMTransfer {
+				origin: ALICE,
+				assets: AssetPair {
+					asset_in: HDX,
+					asset_out: DOT
+				},
+				amount: 1_000_000_000_000,
+				amount_out: 1_000_000_000,
+				discount: false,
+				discount_amount: 0,
+				fee: (1000, 1_000_000),
+			}
+		));
+
+		expect_events(vec![
+			xyk::Event::BuyExecuted(
+				ALICE,
+				DOT,
+				HDX,
+				1_000_000_000_000,
+				1_000_000_000,
+				HDX,
+				1000000,
+				pair_account,
+			)
+			.into(),
+			Event::IntentionResolvedAMMTrade(
+				ALICE,
+				IntentionType::BUY,
+				alice_buy_intention_id,
+				1_000_000_000_000,
+				1_001_000_000,
+				pair_account,
+			)
+			.into(),
+			xyk::Event::SellExecuted(
+				ALICE,
+				HDX,
+				DOT,
+				1_000_000_000_000,
+				1_000_000_000,
+				HDX,
+				1000000,
+				pair_account,
+			)
+			.into(),
+			Event::IntentionResolvedAMMTrade(
+				ALICE,
+				IntentionType::SELL,
+				alice_sell_intention_id,
+				1_000_000_000_000,
+				1_001_000_000,
+				pair_account,
+			)
+			.into(),
+		]);
+	});
+}
+
+// #[test]
+// fn resolve_single_intention_should_work() {
+// 	new_test_ext().execute_with(|| {
+// 		let asset_a = HDX;
+// 		let asset_b = DOT;
+// 		let pool_amount = 1_000_000_000_000_000;
+// 		let initial_price = Price::from_float(0.072);
+// 		initialize_pool(asset_b, asset_a, ALICE, pool_amount, initial_price);
+//
+// 		let alice_buy_intention_id = generate_intention_id(&ALICE, 0);
+// 		Exchange::resolve_single_intention(
+// 			&ExchangeIntention {
+// 				who: ALICE,
+// 				assets: AssetPair { asset_in: DOT, asset_out: HDX },
+// 				amount_in: 19311912264,
+// 				amount_out: 91199318786048,
+// 				trade_limit: 127507368743512,
+// 				discount: false,
+// 				sell_or_buy: IntentionType::BUY,
+// 				intention_id: alice_buy_intention_id }
+// 		);
+//
+// 		let alice_sell_intention_id = generate_intention_id(&ALICE, 1);
+// 		Exchange::resolve_single_intention(
+// 			&ExchangeIntention {
+// 				who: ALICE,
+// 				assets: AssetPair { asset_in: DOT, asset_out: HDX },
+// 				amount_in: 19311912264,
+// 				amount_out: 91199318786048,
+// 				trade_limit: 127507368743512,
+// 				discount: false,
+// 				sell_or_buy: IntentionType::SELL,
+// 				intention_id: alice_sell_intention_id }
+// 		);
+//
+// 		expect_events(vec![
+// 			xyk::Event::BuyExecuted(
+// 				ALICE,
+// 				DOT,
+// 				HDX,
+// 				1_000_000_000_000,
+// 				1_000_000_000,
+// 				HDX,
+// 				1000000,
+// 			)
+// 				.into(),
+// 			Event::IntentionResolvedAMMTrade(
+// 				ALICE,
+// 				IntentionType::BUY,
+// 				alice_buy_intention_id,
+// 				1_000_000_000_000,
+// 				1_001_000_000,
+// 			)
+// 				.into(),
+// 			xyk::Event::SellExecuted(
+// 				ALICE,
+// 				HDX,
+// 				DOT,
+// 				1_000_000_000_000,
+// 				1_000_000_000,
+// 				HDX,
+// 				1000000,
+// 			)
+// 				.into(),
+// 			Event::IntentionResolvedAMMTrade(
+// 				ALICE,
+// 				IntentionType::SELL,
+// 				alice_sell_intention_id,
+// 				1_000_000_000_000,
+// 				1_001_000_000,
+// 			)
+// 				.into(),
+// 		]);
+// 	});
+// }
