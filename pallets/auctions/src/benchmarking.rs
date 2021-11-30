c31d35cd66ae4dabc86fb23263708ae65b501847
Valery Gantchev <v@lery.nl>
2021-11-30 20:56:43 +0100

    leave benchmarks for later

--- pallets/auctions/src/benchmarking.rs
+++ pallets/auctions/src/benchmarking.rs
@@ -8,103 +8,33 @@ use sp_std::vec;
 use frame_benchmarking::{account, benchmarks};
 use frame_system::RawOrigin;
 
-const SEED: u32 = 0;
-
-macro_rules! bvec {
-	($( $x:tt )*) => {
-		vec![$( $x )*].try_into().unwrap()
-	}
-}
-
-fn create_account<T: Config>(name: &'static str, index: u32) -> T::AccountId {
-	let caller: T::AccountId = account(name, index, SEED);
-	caller
-}
-
-// TODO: don't use unwrap_or_default
-
 benchmarks! {
-	create_auction {
-		let caller = create_account::<T>("caller", 0);
+	create {
+	}: {
+	} verify {
+	}
 
-		pallet_nft::Pallet::<T>::create_class(RawOrigin::Signed(caller.clone()).into(), Default::default(), T::Lookup::unlookup(caller.clone()), bvec![0])?;
-		pallet_nft::Pallet::<T>::mint(RawOrigin::Signed(caller.clone()).into(), Default::default(), 0u16.into(), T::Lookup::unlookup(caller.clone()), 10u8, bvec![0])?;
+	update {
+	}: {
+	} verify {
+	}
 
-		let auction_info = AuctionInfo {
-			// TODO: replace with worst case
-			name: "Aukce1".as_bytes().to_vec(),
-			last_bid: None,
-			start: T::BlockNumber::from(1u32),
-			end: T::BlockNumber::from(20u32),
-			owner: caller.clone(),
-			auction_type: AuctionType::English,
-			token: (Default::default(), 0u16.into()),
-			minimal_bid: T::CurrencyBalance::from(T::Balance::from(50u32)).into(),
-		};
-	}: _(RawOrigin::Signed(caller.clone()), auction_info)
-	verify {
+	destroy {
+	}: {
+	} verify {
 	}
 
 	bid {
-		let caller = create_account::<T>("caller", 0);
-		let caller2 = create_account::<T>("caller2", 1);
-
-		pallet_nft::Pallet::<T>::create_class(RawOrigin::Signed(caller.clone()).into(), Default::default(), T::Lookup::unlookup(caller.clone()), bvec![0])?;
-		pallet_nft::Pallet::<T>::mint(RawOrigin::Signed(caller.clone()).into(), Default::default(), 0u16.into(), T::Lookup::unlookup(caller.clone()), 10u8, bvec![0])?;
-
-		let auction_info = AuctionInfo {
-			// TODO: replace with worst case
-			name: "Aukce1".as_bytes().to_vec(),
-			last_bid: None,
-			start: T::BlockNumber::from(0u32),
-			end: T::BlockNumber::from(20u32),
-			owner: caller,
-			auction_type: AuctionType::English,
-			token: (Default::default(), 0u16.into()),
-			minimal_bid: T::CurrencyBalance::from(T::Balance::from(50u32)).into(),
-		};
-		let auction_id = AUCTIONS::<T>::new_auction(auction_info).unwrap_or_default();
-
-	}: _(RawOrigin::Signed(caller2.clone()), auction_id, 1_000_000_u32.into())
-	verify {
-	}
-
-	destroy_auction {
-		let caller = create_account::<T>("caller", 0);
-
-		pallet_nft::Pallet::<T>::create_class(RawOrigin::Signed(caller.clone()).into(), Default::default(), T::Lookup::unlookup(caller.clone()), bvec![0])?;
-		pallet_nft::Pallet::<T>::mint(RawOrigin::Signed(caller.clone()).into(), Default::default(), 0u16.into(), T::Lookup::unlookup(caller.clone()), 10u8, bvec![0])?;
-
-		let auction_info = AuctionInfo {
-			// TODO: replace with worst case
-			name: "Aukce1".as_bytes().to_vec(),
-			last_bid: None,
-			start: T::BlockNumber::from(20u32),
-			end: T::BlockNumber::from(50u32),
-			owner: caller.clone(),
-			auction_type: AuctionType::English,
-			token: (Default::default(), 0u16.into()),
-			minimal_bid: T::CurrencyBalance::from(T::Balance::from(50u32)).into(),
-		};
-		let auction_id = AUCTIONS::<T>::new_auction(auction_info).unwrap_or_default();
-
-	}: _(RawOrigin::Signed(caller.clone()), auction_id)
-	verify {
+	}: {
+	} verify {
 	}
 }
 
 #[cfg(test)]
 mod tests {
-	use super::mock::Test;
-	use crate::tests::new_test_ext;
-	use frame_support::assert_ok;
+	use super::Pallet;
+	use crate::mock::*;
+	use frame_benchmarking::impl_benchmark_test_suite;
 
-	#[test]
-	fn test_benchmarks() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(test_benchmark_create_auction::<Test>());
-			assert_ok!(test_benchmark_bid::<Test>());
-			assert_ok!(test_benchmark_destroy_auction::<Test>());
-		});
-	}
+	impl_benchmark_test_suite!(Pallet, super::ExtBuilder::default().build(), super::Test);
 }
