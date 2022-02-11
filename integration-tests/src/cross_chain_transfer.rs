1457a9f9e40fb98588ff045798547b272ce637a2
green-jay <greenee@email.cz>
2022-02-11 13:19:39 +0100

    wip

--- integration-tests/src/cross_chain_transfer.rs
+++ integration-tests/src/cross_chain_transfer.rs
@@ -12,9 +12,10 @@ use orml_traits::currency::MultiCurrency;
 use primitives::asset::AssetPair;
 use sp_runtime::traits::AccountIdConversion;
 use xcm_emulator::TestExt;
-use hydradx_traits::AMM;
+use hydradx_traits::{AMM, pools::SpotPriceProvider};
 use primitives::Balance;
 use sp_runtime::traits::Zero;
+use pallet_xyk::XYKSpotPrice;
 
 fn last_basilisk_events(n: usize) -> Vec<basilisk_runtime::Event> {
 	frame_system::Pallet::<basilisk_runtime::Runtime>::events()
@@ -202,12 +203,20 @@ fn transfer_insufficient_amount_should_fail() {
 
 #[test]
 fn non_native_fee_payment_works() {
+
+	// comments Martin
+
+	// try from_inner(18decimals)
+
+
 	TestNet::reset();
 
 	Basilisk::execute_with(|| {
 
 		let curr_0 = 0;
 		let curr_1 = 1;
+		let asset_a = 1000;
+		let asset_b = 2000;
 
 		// ------------ BOB ------------
 
@@ -216,10 +225,10 @@ fn non_native_fee_payment_works() {
 			curr_1,
 		));
 
-		let alice_bal = basilisk_runtime::Tokens::free_balance(1, &AccountId::from(BOB));
+		let bob_bal = basilisk_runtime::Tokens::free_balance(1, &AccountId::from(BOB));
 
 		// How to calculate and verify the remaining 462_676_500_000 spent on fees?
-		assert_eq!(alice_bal, 999_537_323_500_000);
+		assert_eq!(bob_bal, 999_537_323_500_000);
 
 		let pair_account = basilisk_runtime::XYK::get_pair_id(AssetPair {
 			asset_in: curr_0,
@@ -228,54 +237,60 @@ fn non_native_fee_payment_works() {
 	
 		let share_token = basilisk_runtime::XYK::share_token(pair_account.clone());
 		
-		assert_ok!(basilisk_runtime::Tokens::set_balance(
-			basilisk_runtime::Origin::root(),
-			pair_account.clone().into(),
-			1,
-			1_000_000_000_000_000_000 * BSX,
-			0,
-		));
+		// assert_ok!(basilisk_runtime::Tokens::set_balance(
+		// 	basilisk_runtime::Origin::root(),
+		// 	pair_account.clone().into(),
+		// 	1,
+		// 	1_000 * BSX,
+		// 	0,
+		// ));
 
-		assert_ok!(basilisk_runtime::Balances::set_balance(
-			basilisk_runtime::Origin::root(),
-			pair_account.clone().into(),
-			1_000_000_000_000_000_000 * BSX,
-			0,
-		));
+		// assert_ok!(basilisk_runtime::Balances::set_balance(
+		// 	basilisk_runtime::Origin::root(),
+		// 	pair_account.clone().into(),
+		// 	1_000 * BSX,
+		// 	0,
+		// ));
 
 		// How does this work and how exactly it affects price?
 		assert_ok!(basilisk_runtime::XYK::create_pool(
 			basilisk_runtime::Origin::signed(ALICE.into()),
-			curr_0,
-			curr_1,
-			2_000_000 * BSX,
-			Price::from(99),
+			curr_0, // 1000 BSX 
+			curr_1, // 500 KSM 500_000_033_400_002
+			1_000 * BSX,
+			Price::from_float(0.5),
 		));
+
+		let spot_price = XYKSpotPrice::<basilisk_runtime::Runtime>::spot_price(curr_0, curr_1); // = 2
 
 		// Why XYK buy does not affect price?
 		assert_ok!(basilisk_runtime::XYK::buy(
 			basilisk_runtime::Origin::signed(ALICE.into()),
 			0,
 			1,
-			66_666_666,
-			1_000_000_000,
+			66 * BSX,
+			1_000 * BSX,
 			false,
 		));
 
 		assert_eq!(basilisk_runtime::XYK::get_pool_assets(&pair_account), Some(vec![curr_0, curr_1]));
 
-		let asset_a_reserve = basilisk_runtime::Tokens::free_balance(curr_0, &pair_account);
-		let asset_b_reserve = basilisk_runtime::Tokens::free_balance(curr_1, &pair_account);
+		let asset_a_reserve = basilisk_runtime::Currencies::free_balance(curr_0, &pair_account);
+		let asset_b_reserve = basilisk_runtime::Currencies::free_balance(curr_1, &pair_account);
 
-		// Why spot price = 0?
-		let spot_price = hydra_dx_math::xyk::calculate_spot_price(asset_a_reserve, asset_b_reserve, 1_000 * BSX)
-			.unwrap_or_else(|_| Balance::zero());
+		
 
 		println!("pair_account: {:?}", pair_account);
 		println!("share_token: {:?}", share_token);
 		println!("asset_a_reserve: {:?}", asset_a_reserve);
 		println!("asset_b_reserve: {:?}", asset_b_reserve);
 		println!("spot_price: {:?}", spot_price);	
+
+		let price_from_1 = Price::from_inner(1);
+		println!("price_from_1: {:?}", price_from_1);
+
+		let next = basilisk_runtime::AssetRegistry::next_asset_id();
+		println!("next: {:?}", next);	
 		
 		// ------------ DAVE ------------
 		
@@ -285,35 +300,38 @@ fn non_native_fee_payment_works() {
 		));
 
 		let dave_bal = basilisk_runtime::Tokens::free_balance(1, &AccountId::from(DAVE));
-		assert_eq!(dave_bal, 999_537_323_499_910);
+		//assert_eq!(dave_bal, 999_537_323_499_910);
 
 		let expected_diff = 90;
-		assert_eq!(dave_bal, alice_bal - expected_diff);
+		//assert_eq!(dave_bal, bob_bal - expected_diff);
 
 		// Is this correct oracle setup?
 		pub const PRICE_ENTRY_1: PriceEntry = PriceEntry {
-			price: Price::from_inner(2000000000000000000),
+			price: Price::from_inner(5000000000000000000),
 			trade_amount: 1_000,
 			liquidity_amount: 2_000,
 		};
 
-		for i in 1..1000 {
+		for i in 1..11 {
             basilisk_runtime::PriceOracle::on_initialize(i);
             basilisk_runtime::System::set_block_number(i.into());
             basilisk_runtime::PriceOracle::on_trade(curr_0, curr_1, PRICE_ENTRY_1);
             basilisk_runtime::PriceOracle::on_finalize(i.into());
         }
 
-		let hdx_dot_pair_name = basilisk_runtime::PriceOracle::get_name(curr_0, curr_1);
-		let data_thousand = basilisk_runtime::PriceOracle::price_data_thousand(hdx_dot_pair_name.clone());
+		let pair_name = basilisk_runtime::PriceOracle::get_name(curr_0, curr_1);
+		// println!("pair_name: {:?}", pair_name);
+		let data_ten = basilisk_runtime::PriceOracle::price_data_ten();
+		let data_thousand = basilisk_runtime::PriceOracle::price_data_thousand(pair_name.clone());
 		println!("data_thousand: {:?}", data_thousand);
+		//println!("data_thousand: {:?}", data_thousand);
 
 		expect_basilisk_events(vec![
 			pallet_transaction_multi_payment::Event::FeeWithdrawn(
 				DAVE.into(),
 				1,
 				462_676_500_000,
-				462_676_500_090,
+				231_338_280_875,
 				FALLBACK.into(),
 			)
 			.into(),
