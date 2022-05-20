63a12e874d2c18f0d711a9fc21c432beb011b3d7
Martin Hloska <martin.hloska@gmail.com>
2022-05-20 17:37:41 +0200

    add invariant test for in given out

--- pallets/stablewap/src/tests/invariants.rs
+++ pallets/stablewap/src/tests/invariants.rs
@@ -1,4 +1,4 @@
-use crate::math::{calculate_d, calculate_out_given_in};
+use crate::math::{calculate_d, calculate_in_given_out, calculate_out_given_in};
 use crate::Balance;
 use proptest::prelude::*;
 use proptest::proptest;
@@ -37,5 +37,29 @@ proptest! {
 		let _d2 = calculate_d(&[reserve_in + amount_in, reserve_out - result.unwrap() ], ann, precision).unwrap();
 
 		//assert!(d2 >= d1);
+	}
+}
+
+proptest! {
+	#![proptest_config(ProptestConfig::with_cases(1000))]
+	#[test]
+	fn test_in_given_out(amount_out in trade_amount(),
+		reserve_in in  asset_reserve(),
+		reserve_out in  asset_reserve(),
+	) {
+		let amp = 1u128;
+		let ann = 4u128;
+
+		let precision = 1u128;
+
+		let _d1 = calculate_d(&[reserve_in, reserve_out], ann, precision).unwrap();
+
+		let result = calculate_in_given_out(reserve_in, reserve_out, amount_out, precision, amp);
+
+		assert!(result.is_some());
+
+		let _d2 = calculate_d(&[reserve_in + result.unwrap(), reserve_out - amount_out ], ann, precision).unwrap();
+
+		//assert!(_d2 >= _d1);
 	}
 }
