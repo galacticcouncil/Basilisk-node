b2e30717cb5ac85d2132302cf5c0b4cc87ea39a0
Alexander Popiak <alexander.popiak@parity.io>
2022-06-07 12:54:51 +0200

    add integration test for automatic fee currency on xcm transfer

--- integration-tests/src/non_native_fee.rs
+++ integration-tests/src/non_native_fee.rs
@@ -152,30 +152,28 @@ fn fee_currency_on_account_lifecycle() {
 	const HITCHHIKER: [u8; 32] = [42u8; 32];
 
 	Basilisk::execute_with(|| {
-		let currency_1 = 1;
-		let acc_id = AccountId::from(HITCHHIKER);
 
-		assert_eq!(basilisk_runtime::MultiTransactionPayment::get_currency(&acc_id), None);
+		assert_eq!(basilisk_runtime::MultiTransactionPayment::get_currency(&AccountId::from(HITCHHIKER)), None);
 
 		// ------------ set on create ------------
 		assert_ok!(basilisk_runtime::Currencies::transfer(
 			basilisk_runtime::Origin::signed(BOB.into()),
 			HITCHHIKER.into(),
-			currency_1,
+			1,
 			50_000_000_000_000,
 		));
 
-		assert_eq!(basilisk_runtime::Tokens::free_balance(1, &acc_id), 50_000_000_000_000);
-		assert_eq!(basilisk_runtime::MultiTransactionPayment::get_currency(&acc_id), Some(currency_1));
+		assert_eq!(basilisk_runtime::Tokens::free_balance(1, &AccountId::from(HITCHHIKER)), 50_000_000_000_000);
+		assert_eq!(basilisk_runtime::MultiTransactionPayment::get_currency(&AccountId::from(HITCHHIKER)), Some(1));
 
 		// ------------ remove on delete ------------
 		assert_ok!(basilisk_runtime::Tokens::transfer_all(
 			basilisk_runtime::Origin::signed(HITCHHIKER.into()),
 			BOB.into(),
-			currency_1,
+			1,
 			false,
 		));
 
-		assert_eq!(basilisk_runtime::MultiTransactionPayment::get_currency(&acc_id), None);
+		assert_eq!(basilisk_runtime::MultiTransactionPayment::get_currency(&AccountId::from(HITCHHIKER)), None);
 	});
 }
