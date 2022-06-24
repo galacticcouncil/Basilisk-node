7242bde42a8cc76839e41e02868839800d9c4505
Martin <martin.hloska@gmail.com>
2022-06-24 10:49:39 +0200

    bump runtime version

--- runtime/testing-basilisk/src/lib.rs
+++ runtime/testing-basilisk/src/lib.rs
@@ -113,7 +113,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("testing-basilisk"),
 	impl_name: create_runtime_str!("testing-basilisk"),
 	authoring_version: 1,
-	spec_version: 57,
+	spec_version: 58,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
