7f8ae32778eecc28eaca42e9f8d7212a979c00ae
Alexander Popiak <alexander.popiak@parity.io>
2022-07-01 17:13:22 +0200

    add doc comment and test for AllowEverythingExcept

--- runtime/common/src/filters.rs
+++ runtime/common/src/filters.rs
@@ -4,6 +4,7 @@ use polkadot_xcm::latest::prelude::*;
 use sp_std::marker::PhantomData;
 use sp_std::vec::Vec;
 
+/// A filter that allows all assets except assets with and id corresponding to the excluded location.
 pub struct AllowEverythingExcept<ExcludedLocation>(PhantomData<ExcludedLocation>);
 impl<ExcludedLocation: Get<MultiLocation>> Contains<(MultiLocation, Vec<MultiAsset>)>
 	for AllowEverythingExcept<ExcludedLocation>
@@ -12,5 +13,30 @@ impl<ExcludedLocation: Get<MultiLocation>> Contains<(MultiLocation, Vec<MultiAss
 		!assets
 			.iter()
 			.any(|asset| asset.is_fungible(Some(Concrete(ExcludedLocation::get()))))
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn allow_everything_except() {
+		frame_support::parameter_types! {
+			pub ParentLocation: MultiLocation = MultiLocation::parent();
+		}
+		type DontAllowParent = AllowEverythingExcept<ParentLocation>;
+
+		assert!(DontAllowParent::contains(&(
+			MultiLocation::here(),
+			vec![
+				(MultiLocation::here(), 1234).into(),
+				(MultiLocation::new(1, X1(Parachain(2000))), 42).into()
+			]
+		)));
+		assert!(!DontAllowParent::contains(&(
+			MultiLocation::here(),
+			vec![(MultiLocation::parent(), 1234).into()]
+		)))
 	}
 }
