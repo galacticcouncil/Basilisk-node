2e7bfe29fee910b79905f42c48da15c3587454d3
mrq <mrq@mrq.cz>
2022-07-08 20:48:44 +0200

    fmt

--- pallets/marketplace/src/tests/make_offer.rs
+++ pallets/marketplace/src/tests/make_offer.rs
@@ -3,20 +3,20 @@ use pretty_assertions::assert_eq;
 
 #[test]
 fn make_offer_should_work_when_nft_exists() {
-    new_test_ext().execute_with(|| {
-        // arrange
-        let metadata: BoundedVec<u8, <Test as pallet_uniques::Config>::StringLimit> =
-            b"metadata".to_vec().try_into().unwrap();
-        assert_ok!(NFT::create_class(
+	new_test_ext().execute_with(|| {
+		// arrange
+		let metadata: BoundedVec<u8, <Test as pallet_uniques::Config>::StringLimit> =
+			b"metadata".to_vec().try_into().unwrap();
+		assert_ok!(NFT::create_class(
 			Origin::signed(ALICE),
 			CLASS_ID_0,
 			Default::default(),
 			metadata.clone()
 		));
-        assert_ok!(NFT::mint(Origin::signed(ALICE), CLASS_ID_0, INSTANCE_ID_0, metadata));
+		assert_ok!(NFT::mint(Origin::signed(ALICE), CLASS_ID_0, INSTANCE_ID_0, metadata));
 
-        // act
-        assert_ok!(Market::make_offer(
+		// act
+		assert_ok!(Market::make_offer(
 			Origin::signed(CHARLIE),
 			CLASS_ID_0,
 			INSTANCE_ID_0,
@@ -24,42 +24,42 @@ fn make_offer_should_work_when_nft_exists() {
 			2
 		));
 
-        // assert
-        assert_eq!(
-            Market::offers((CLASS_ID_0, INSTANCE_ID_0), CHARLIE),
-            Some(Offer {
-                maker: CHARLIE,
-                amount: 50 * UNITS,
-                expires: 2,
-            })
-        );
-        let event = Event::Marketplace(crate::Event::OfferPlaced {
-            who: CHARLIE,
-            class: CLASS_ID_0,
-            instance: INSTANCE_ID_0,
-            amount: 50 * UNITS,
-            expires: 2,
-        });
-        assert_eq!(last_event(), event);
-    });
+		// assert
+		assert_eq!(
+			Market::offers((CLASS_ID_0, INSTANCE_ID_0), CHARLIE),
+			Some(Offer {
+				maker: CHARLIE,
+				amount: 50 * UNITS,
+				expires: 2,
+			})
+		);
+		let event = Event::Marketplace(crate::Event::OfferPlaced {
+			who: CHARLIE,
+			class: CLASS_ID_0,
+			instance: INSTANCE_ID_0,
+			amount: 50 * UNITS,
+			expires: 2,
+		});
+		assert_eq!(last_event(), event);
+	});
 }
 
 #[test]
 fn make_offer_should_fail_when_offer_is_lower_than_minimal_amount() {
-    new_test_ext().execute_with(|| {
-        // arrange
-        let metadata: BoundedVec<u8, <Test as pallet_uniques::Config>::StringLimit> =
-            b"metadata".to_vec().try_into().unwrap();
-        assert_ok!(NFT::create_class(
+	new_test_ext().execute_with(|| {
+		// arrange
+		let metadata: BoundedVec<u8, <Test as pallet_uniques::Config>::StringLimit> =
+			b"metadata".to_vec().try_into().unwrap();
+		assert_ok!(NFT::create_class(
 			Origin::signed(ALICE),
 			CLASS_ID_0,
 			Default::default(),
 			metadata.clone()
 		));
-        assert_ok!(NFT::mint(Origin::signed(ALICE), CLASS_ID_0, INSTANCE_ID_0, metadata));
+		assert_ok!(NFT::mint(Origin::signed(ALICE), CLASS_ID_0, INSTANCE_ID_0, metadata));
 
-        // act & assert
-        assert_noop!(
+		// act & assert
+		assert_noop!(
 			Market::make_offer(
 				Origin::signed(BOB),
 				CLASS_ID_0,
@@ -69,6 +69,6 @@ fn make_offer_should_fail_when_offer_is_lower_than_minimal_amount() {
 			),
 			Error::<Test>::OfferTooLow
 		);
-        assert_eq!(Market::offers((CLASS_ID_0, INSTANCE_ID_0), CHARLIE), None);
-    });
+		assert_eq!(Market::offers((CLASS_ID_0, INSTANCE_ID_0), CHARLIE), None);
+	});
 }
--- pallets/marketplace/src/tests/mod.rs
+++ pallets/marketplace/src/tests/mod.rs
@@ -8,10 +8,10 @@ use std::convert::TryInto;
 type Market = Pallet<Test>;
 
 fn new_test_ext() -> sp_io::TestExternalities {
-    let mut ext = ExtBuilder::default().build();
-    ext.execute_with(|| System::set_block_number(1));
-    ext
+	let mut ext = ExtBuilder::default().build();
+	ext.execute_with(|| System::set_block_number(1));
+	ext
 }
 
-mod unit;
 mod make_offer;
+mod unit;
