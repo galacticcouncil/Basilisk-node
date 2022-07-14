efdaf459034bd1e0d7504398e333d548bcaf2df9
Alexander Popiak <alexander.popiak@parity.io>
2022-07-14 11:23:20 -0400

    add integration test for asset trapping on unknown asset and use patched polkadot version

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -3,3 +3,17 @@ mod fees;
 mod kusama_test_net;
 mod nft_marketplace;
 mod non_native_fee;
+
+use polkadot_xcm::{latest::prelude::*, VersionedMultiAssets};
+use sp_core::H256;
+use sp_runtime::traits::{BlakeTwo256, Hash};
+
+// Determine the hash for assets expected to be have been trapped.
+#[allow(unused)]
+pub(crate) fn determine_hash<M>(origin: &MultiLocation, assets: M) -> H256
+where
+	M: Into<MultiAssets>,
+{
+	let versioned = VersionedMultiAssets::from(assets.into());
+	BlakeTwo256::hash_of(&(origin, &versioned))
+}
