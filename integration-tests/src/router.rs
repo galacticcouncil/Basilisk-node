f4340d0dbb171855bf1320a9f8f65cd8d090a2b3
dmoka <mokabme@gmail.com>
2022-08-23 11:14:37 +0200

    add integration tests for execute single and multiple trades of routes

--- integration-tests/src/router.rs
+++ integration-tests/src/router.rs
@@ -3,7 +3,7 @@
 use crate::kusama_test_net::*;
 
 use basilisk_runtime::{Origin, Router, XYK};
-use primitives::Price;
+use primitives::{Price};
 use xcm_emulator::TestExt;
 
 use sp_arithmetic::fixed_point::FixedPointNumber;
@@ -13,51 +13,107 @@ use hydradx_traits::router::PoolType;
 use orml_traits::MultiCurrency;
 use pallet_router::types::Trade;
 
+const BSX: u32 = 1;
+const AUSD: u32 = 2;
+const MOVR: u32 = 3;
+const KSM: u32 = 4;
+
+const TRADER: [u8; 32] = BOB;
+
 #[test]
-fn router() {
+fn execute_sell_should_work_when_route_contains_single_trade() {
 	TestNet::reset();
 
-	let asset_a = 1;
-	let asset_b = 2;
-	let asset_c = 3;
-
 	Basilisk::execute_with(|| {
-		//arrange
-		assert_ok!(XYK::create_pool(
-			Origin::signed(ALICE.into()),
-			asset_a,
-			asset_b,
-			100 * UNITS,
-			Price::checked_from_rational(1, 2).unwrap()
-		));
-		assert_ok!(XYK::create_pool(
-			Origin::signed(ALICE.into()),
-			asset_b,
-			asset_c,
-			100 * UNITS,
-			Price::checked_from_rational(1, 3).unwrap()
-		));
+		//Arrange
+		assert_trader_balance(0, AUSD);
+		create_pool(BSX, AUSD);
+
+		let amount_to_sell = 10;
+		let limit = 0;
+		let trades = vec![
+			Trade {
+				pool: PoolType::XYK,
+				asset_in: BSX,
+				asset_out: AUSD,
+			}
+		];
+
+		//Act
 		assert_ok!(Router::execute_sell(
-			Origin::signed(BOB.into()),
-			asset_a,
-			asset_c,
-			10 * UNITS,
-			0u128,
-			vec![
-				Trade {
-					pool: PoolType::XYK,
-					asset_in: asset_a,
-					asset_out: asset_b,
-				},
-				Trade {
-					pool: PoolType::XYK,
-					asset_in: asset_b,
-					asset_out: asset_c,
-				}
-			]
+			Origin::signed(TRADER.into()),
+			BSX,
+			AUSD,
+			amount_to_sell * UNITS,
+			limit,
+			trades
 		));
 
-		let bob_balance = basilisk_runtime::Tokens::free_balance(3, &AccountId::from(BOB));
-		assert_eq!(bob_balance, 144_078_068_1540u128);
+		//Assert
+		assert_trader_balance(453_181_818_1819u128, AUSD);
 	});
 }
+
+#[test]
+fn execute_sell_should_work_when_route_contains_multiple_trades() {
+	TestNet::reset();
+
+	Basilisk::execute_with(|| {
+		//Arrange
+		assert_trader_balance(0, AUSD);
+
+		create_pool(BSX, AUSD);
+		create_pool(AUSD, MOVR);
+		create_pool(MOVR, KSM);
+
+		let amount_to_sell = 10;
+		let limit = 0;
+		let trades = vec![
+			Trade {
+				pool: PoolType::XYK,
+				asset_in: BSX,
+				asset_out: AUSD,
+			},
+			Trade {
+				pool: PoolType::XYK,
+				asset_in: AUSD,
+				asset_out: MOVR,
+			},
+			Trade {
+				pool: PoolType::XYK,
+				asset_in: MOVR,
+				asset_out: KSM,
+			}
+		];
+
+		assert_ok!(Router::execute_sell(
+			Origin::signed(TRADER.into()),
+			BSX,
+			KSM,
+			amount_to_sell * UNITS,
+			limit,
+			trades
+		));
+
+		assert_trader_balance(105_455_305_9484u128, KSM);
+	});
+}
+
+//TODO: add test for when there is no existing pool
+//TODO: add test for limit
+
+fn create_pool(asset_a: u32, asset_b: u32) {
+	assert_ok!(XYK::create_pool(
+		Origin::signed(ALICE.into()),
+		asset_a,
+		asset_b,
+		100 * UNITS,
+		Price::checked_from_rational(1, 2).unwrap()
+	));
+}
+
+fn assert_trader_balance(balance: u128, asset_id: u32, ) {
+	let bob_balance = basilisk_runtime::Tokens::free_balance(asset_id, &AccountId::from(TRADER));
+	assert_eq!(bob_balance, balance);
+}
+
