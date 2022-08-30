f24b875ae20d22d243cca61a2a24ccb0e930213d
Roznovjak <richardroznovjak@gmail.com>
2022-08-30 12:23:53 +0200

    renaming

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -50,11 +50,11 @@ pub type Amount = i128;
 /// Price
 pub type Price = FixedU128;
 
-/// NFT Class ID
-pub type ClassId = u128;
+/// NFT Collection ID
+pub type CollectionId = u128;
 
-/// NFT Instance ID
-pub type InstanceId = u128;
+/// NFT Item ID
+pub type ItemId = u128;
 
 /// Scaled Unsigned of Balance
 pub type HighPrecisionBalance = U256;
--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -276,7 +276,7 @@ parameter_types! {
 	pub const LMPalletId: PalletId = PalletId(*b"LiqMinId");
 	pub const MinPlannedYieldingPeriods: BlockNumber = 100;
 	pub const MinTotalFarmRewards: Balance = NATIVE_EXISTENTIAL_DEPOSIT * 1_000;
-	pub const NftClass: primitives::ClassId = 1;
+	pub const NftClass: primitives::CollectionId = 1;
 }
 
 // pallet identity
