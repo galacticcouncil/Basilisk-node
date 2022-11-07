5bc1c181c176a3ad9fdcf62666df0e3507d690fc
martinfridrich <mrtnfridrich@gmail.com>
2022-11-07 18:17:15 +0100

    removed marketplace migrations + removed all migrations from runtime

--- runtime/basilisk/src/migrations.rs
+++ runtime/basilisk/src/migrations.rs
@@ -13,39 +13,15 @@ pub struct OnRuntimeUpgradeMigration;
 impl OnRuntimeUpgrade for OnRuntimeUpgradeMigration {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<(), &'static str> {
-		pallet_marketplace::migration::v2::pre_migrate::<Runtime>();
-
-		frame_support::log::info!("PreMigrate NFT Pallet start");
-		pallet_nft::migration::v1::pre_migrate::<Runtime>();
-		frame_support::log::info!("PreMigrate NFT Pallet end");
-
 		Ok(())
 	}
 
 	fn on_runtime_upgrade() -> Weight {
-		let mut weight: Weight = Weight::zero();
-
-		frame_support::log::info!("Migrate Uniques Pallet start");
-		weight = weight.saturating_add(<MigrateUniquesPallet as OnRuntimeUpgrade>::on_runtime_upgrade());
-		frame_support::log::info!("Migrate Uniques Pallet end");
-
-		weight = weight.saturating_add(pallet_marketplace::migration::v2::migrate::<Runtime>());
-
-		frame_support::log::info!("Migrate NFT Pallet start");
-		weight = weight.saturating_add(pallet_nft::migration::v1::migrate::<Runtime>());
-		frame_support::log::info!("Migrate NFT Pallet end");
-
-		weight
+		Weight::zero()
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade() -> Result<(), &'static str> {
-		pallet_marketplace::migration::v2::post_migrate::<Runtime>();
-
-		frame_support::log::info!("PostMigrate NFT Pallet start");
-		pallet_nft::migration::v1::post_migrate::<Runtime>();
-		frame_support::log::info!("PostMigrate NFT Pallet end");
-
 		Ok(())
 	}
 }
