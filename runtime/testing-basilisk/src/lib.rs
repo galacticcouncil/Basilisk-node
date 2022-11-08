2a99d36dea82c71acd8f4b071fdb78803dbd752e
Lumir Mrkva <mrq@mrq.cz>
2022-11-08 04:27:40 +0100

    increased MaxInvulnerables

--- runtime/testing-basilisk/src/lib.rs
+++ runtime/testing-basilisk/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("testing-basilisk"),
 	impl_name: create_runtime_str!("testing-basilisk"),
 	authoring_version: 1,
-	spec_version: 81,
+	spec_version: 85,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
