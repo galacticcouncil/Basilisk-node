54d2613e6ba2d2f139d24e7adaf7b16c49ebaa0e
Alexander Popiak <alexander.popiak@parity.io>
2023-03-10 14:00:24 +0100

    bump crate and spec versions

--- runtime/testing-basilisk/src/lib.rs
+++ runtime/testing-basilisk/src/lib.rs
@@ -130,7 +130,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("testing-basilisk"),
 	impl_name: create_runtime_str!("testing-basilisk"),
 	authoring_version: 1,
-	spec_version: 93,
+	spec_version: 94,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
