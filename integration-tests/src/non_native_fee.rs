5babd3ced6f9f8cfe9353772b9b322f70786f407
Alexander Popiak <alexander.popiak@parity.io>
2023-05-25 11:14:41 +0200

    use BSX and AUSD constants

--- integration-tests/src/non_native_fee.rs
+++ integration-tests/src/non_native_fee.rs
@@ -39,11 +39,8 @@ fn non_native_fee_payment_works_with_xyk_spot_price() {
 	TestNet::reset();
 
 	Basilisk::execute_with(|| {
-		let currency_0 = 0;
-		let currency_1 = 1;
-
 		let call = basilisk_runtime::RuntimeCall::MultiTransactionPayment(
-			pallet_transaction_multi_payment::Call::set_currency { currency: currency_1 },
+			pallet_transaction_multi_payment::Call::set_currency { currency: AUSD },
 		);
 
 		let info = DispatchInfo {
@@ -61,12 +58,12 @@ fn non_native_fee_payment_works_with_xyk_spot_price() {
 			)
 		);
 
-		let bob_balance = basilisk_runtime::Tokens::free_balance(1, &AccountId::from(BOB));
+		let bob_balance = basilisk_runtime::Tokens::free_balance(AUSD, &AccountId::from(BOB));
 		assert_eq!(bob_balance, 999_999_992_137_669);
 
 		let pair_account = basilisk_runtime::XYK::get_pair_id(AssetPair {
-			asset_in: currency_0,
-			asset_out: currency_1,
+			asset_in: BSX,
+			asset_out: AUSD,
 		});
 
 		assert_ok!(basilisk_runtime::Balances::set_balance(
@@ -79,28 +76,28 @@ fn non_native_fee_payment_works_with_xyk_spot_price() {
 		assert_ok!(basilisk_runtime::Tokens::set_balance(
 			basilisk_runtime::RuntimeOrigin::root(),
 			ALICE.into(),
-			1,
+			AUSD,
 			2_000_000_000_000 * UNITS,
 			0,
 		));
 
 		assert_ok!(basilisk_runtime::XYK::create_pool(
 			basilisk_runtime::RuntimeOrigin::signed(ALICE.into()),
-			currency_0, // 1000 BSX
+			BSX, // 1000 BSX
 			1_000 * UNITS,
-			currency_1, // 500 KSM (500_000_033_400_002)
+			AUSD, // 500 AUSD
 			500 * UNITS,
 		));
 
-		let spot_price = XYKSpotPrice::<basilisk_runtime::Runtime>::spot_price(currency_0, currency_1);
+		let spot_price = XYKSpotPrice::<basilisk_runtime::Runtime>::spot_price(BSX, AUSD);
 		assert_eq!(spot_price, Some(Price::from_float(0.5)));
 
 		basilisk_run_to_block(2);
 
 		assert_ok!(basilisk_runtime::XYK::buy(
 			basilisk_runtime::RuntimeOrigin::signed(ALICE.into()),
-			0,
-			1,
+			BSX,
+			AUSD,
 			66 * UNITS,
 			1_000 * UNITS,
 			false,
@@ -110,12 +107,12 @@ fn non_native_fee_payment_works_with_xyk_spot_price() {
 
 		assert_eq!(
 			basilisk_runtime::XYK::get_pool_assets(&pair_account),
-			Some(vec![currency_0, currency_1])
+			Some(vec![BSX, AUSD])
 		);
 
 		// ------------ DAVE ------------
 		let call = basilisk_runtime::RuntimeCall::MultiTransactionPayment(
-			pallet_transaction_multi_payment::Call::set_currency { currency: currency_1 },
+			pallet_transaction_multi_payment::Call::set_currency { currency: AUSD },
 		);
 
 		assert_ok!(
