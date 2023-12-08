23db9273890ecd79f031e25a575a5617e1b0960e
dmoka <mokabme@gmail.com>
2023-12-08 10:09:43 +0100

    add integration test to verify if exchange asset works with onchain routing

--- integration-tests/src/exchange_asset.rs
+++ integration-tests/src/exchange_asset.rs
@@ -1,11 +1,15 @@
 #![cfg(test)]
 
 use crate::kusama_test_net::*;
+use basilisk_runtime::Currencies;
 use basilisk_runtime::RuntimeOrigin;
 use basilisk_runtime::XYK;
 use frame_support::dispatch::GetDispatchInfo;
 use frame_support::weights::Weight;
 use frame_support::{assert_ok, pallet_prelude::*};
+use hydradx_traits::router::AssetPair;
+use hydradx_traits::router::PoolType;
+use hydradx_traits::router::Trade;
 use orml_traits::currency::MultiCurrency;
 use polkadot_xcm::{latest::prelude::*, VersionedXcm};
 use pretty_assertions::assert_eq;
@@ -141,6 +145,91 @@ fn basilisk_should_swap_assets_when_receiving_from_otherchain_with_buy() {
 	});
 }
 
+#[test]
+fn basilisk_should_swap_assets_coming_from_karura_when_onchain_route_present() {
+	//Arrange
+	TestNet::reset();
+
+	Basilisk::execute_with(|| {
+		register_kar();
+		assert_ok!(basilisk_runtime::Tokens::deposit(KAR, &CHARLIE.into(), 3000 * UNITS));
+		create_xyk_pool_with_amounts(KAR, 100000 * UNITS, BSX, 100000 * UNITS);
+		create_xyk_pool_with_amounts(BSX, 100000 * UNITS, KSM, 100000 * UNITS);
+
+		//Register KSM location
+		assert_ok!(basilisk_runtime::AssetRegistry::set_location(
+			basilisk_runtime::RuntimeOrigin::root(),
+			KSM,
+			basilisk_runtime::AssetLocation(MultiLocation::new(0, X1(GeneralIndex(3))))
+		));
+
+		//Register onchain route from KAR to KSM
+		assert_ok!(basilisk_runtime::Router::set_route(
+			RuntimeOrigin::signed(CHARLIE.into()),
+			AssetPair::new(KAR, KSM),
+			vec![
+				Trade {
+					pool: PoolType::XYK,
+					asset_in: KAR,
+					asset_out: BSX,
+				},
+				Trade {
+					pool: PoolType::XYK,
+					asset_in: BSX,
+					asset_out: KSM,
+				},
+			],
+		));
+
+		add_currency_price(KAR, FixedU128::from(1));
+	});
+
+	OtherParachain::execute_with(|| {
+		let xcm = craft_exchange_asset_xcm::<_, basilisk_runtime::RuntimeCall>(
+			MultiAsset::from((GeneralIndex(0), 5 * UNITS)),
+			MultiAsset::from((GeneralIndex(KSM.into()), 2 * UNITS)),
+			SELL,
+		);
+		//Act
+		let res = basilisk_runtime::PolkadotXcm::execute(
+			basilisk_runtime::RuntimeOrigin::signed(ALICE.into()),
+			Box::new(xcm),
+			Weight::from_parts(399_600_000_000, 0),
+		);
+		assert_ok!(res);
+
+		//Assert
+		assert_eq!(
+			basilisk_runtime::Balances::free_balance(AccountId::from(ALICE)),
+			ALICE_INITIAL_NATIVE_BALANCE_ON_OTHER_PARACHAIN - 100 * UNITS
+		);
+
+		assert!(matches!(
+			last_other_para_events(2).first(),
+			Some(basilisk_runtime::RuntimeEvent::XcmpQueue(
+				cumulus_pallet_xcmp_queue::Event::XcmpMessageSent { .. }
+			))
+		));
+	});
+
+	let fees = 27_500_000_000_000;
+	Basilisk::execute_with(|| {
+		assert_eq!(
+			basilisk_runtime::Tokens::free_balance(KAR, &AccountId::from(BOB)),
+			95000000000000 - fees
+		);
+		let received = 4969548790555;
+		assert_eq!(
+			basilisk_runtime::Tokens::free_balance(KSM, &AccountId::from(BOB)),
+			received
+		);
+		assert_eq!(
+			basilisk_runtime::Tokens::free_balance(KAR, &basilisk_runtime::Treasury::account_id()),
+			fees
+		);
+	});
+}
+
 fn register_kar() {
 	assert_ok!(basilisk_runtime::AssetRegistry::register(
 		basilisk_runtime::RuntimeOrigin::root(),
@@ -254,5 +343,28 @@ fn create_xyk_pool(asset_a: u32, asset_b: u32) {
 		100 * UNITS,
 		asset_b,
 		50 * UNITS,
+	));
+}
+
+fn create_xyk_pool_with_amounts(asset_a: u32, amount_a: u128, asset_b: u32, amount_b: u128) {
+	assert_ok!(Currencies::update_balance(
+		RuntimeOrigin::root(),
+		DAVE.into(),
+		asset_a,
+		amount_a as i128,
+	));
+
+	assert_ok!(Currencies::update_balance(
+		RuntimeOrigin::root(),
+		DAVE.into(),
+		asset_b,
+		amount_b as i128,
+	));
+	assert_ok!(XYK::create_pool(
+		RuntimeOrigin::signed(DAVE.into()),
+		asset_a,
+		amount_a,
+		asset_b,
+		amount_b,
 	));
 }
