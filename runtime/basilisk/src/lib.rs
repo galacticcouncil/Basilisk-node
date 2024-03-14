97bec6f9ff98128a05631af9733ec8ad92f67a14
Roznovjak <richardroznovjak@gmail.com>
2024-03-14 12:12:08 -0500

    bump runtime version

--- runtime/basilisk/src/lib.rs
+++ runtime/basilisk/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("basilisk"),
 	impl_name: create_runtime_str!("basilisk"),
 	authoring_version: 1,
-	spec_version: 111,
+	spec_version: 112,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
