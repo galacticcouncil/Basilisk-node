57306de44953029ebdba3874968ab78665090290
Valery Gantchev <v@lery.nl>
2024-08-30 17:08:01 +0200

    lic

--- node/src/chain_spec/rococo.rs
+++ node/src/chain_spec/rococo.rs
@@ -1,4 +1,4 @@
-// This file is part of pallet-asset-registry.
+// This file is part of Basilisk-node
 
 // Copyright (C) 2020-2022  Intergalactic, Limited (GIB).
 // SPDX-License-Identifier: Apache-2.0
