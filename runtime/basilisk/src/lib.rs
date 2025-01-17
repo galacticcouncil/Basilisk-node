6118d7b9a89adb110fa5d2e64388c16022bd6a6e
Roznovjak <richardroznovjak@gmail.com>
2025-01-17 09:15:34 +0100

    update migrations

--- runtime/basilisk/src/lib.rs
+++ runtime/basilisk/src/lib.rs
@@ -48,7 +48,6 @@ pub use governance::*;
 pub use system::*;
 pub use xcm::*;
 
-use frame_support::parameter_types;
 use frame_support::sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
 	traits::{AccountIdConversion, BlakeTwo256, Block as BlockT},
@@ -273,55 +272,13 @@ pub type Executive = frame_executive::Executive<
 
 pub mod migrations {
 	use super::*;
-	use frame_support::traits::LockIdentifier;
-	use frame_system::pallet_prelude::BlockNumberFor;
 
-	parameter_types! {
-		pub const CouncilPalletName: &'static str = "Council";
-		// pub const TechnicalCommitteePalletName: &'static str = "TechnicalCommittee";
-		pub const PhragmenElectionPalletName: &'static str = "Elections";
-		// pub const TechnicalMembershipPalletName: &'static str = "TechnicalMembership";
-		pub const TipsPalletName: &'static str = "Tips";
-		pub const PhragmenElectionPalletId: LockIdentifier = *b"phrelect";
-		pub const DataDepositPerByte: Balance = primitives::constants::currency::CENTS;
-		pub const TipReportDepositBase: Balance = 10 * primitives::constants::currency::DOLLARS;
-	}
-
-	// Special Config for Gov V1 pallets, allowing us to run migrations for them without
-	// implementing their configs on [`Runtime`].
-	pub struct UnlockConfig;
-	impl pallet_elections_phragmen::migrations::unlock_and_unreserve_all_funds::UnlockConfig for UnlockConfig {
-		type Currency = Balances;
-		type MaxVotesPerVoter = ConstU32<16>;
-		type PalletId = PhragmenElectionPalletId;
-		type AccountId = AccountId;
-		type DbWeight = <Runtime as frame_system::Config>::DbWeight;
-		type PalletName = PhragmenElectionPalletName;
-	}
-	impl pallet_tips::migrations::unreserve_deposits::UnlockConfig<()> for UnlockConfig {
-		type Currency = Balances;
-		type Hash = Hash;
-		type DataDepositPerByte = DataDepositPerByte;
-		type TipReportDepositBase = TipReportDepositBase;
-		type AccountId = AccountId;
-		type BlockNumber = BlockNumberFor<Runtime>;
-		type DbWeight = <Runtime as frame_system::Config>::DbWeight;
-		type PalletName = TipsPalletName;
+	impl cumulus_pallet_xcmp_queue::migration::v5::V5Config for Runtime {
+		type ChannelList = ParachainSystem;
 	}
 
 	pub type Migrations = (
-		pallet_collator_selection::migration::v2::MigrationToV2<Runtime>,
-		// Unlock/unreserve balances from Gov v1 pallets that hold them
-		// https://github.com/paritytech/polkadot/issues/6749
-		pallet_elections_phragmen::migrations::unlock_and_unreserve_all_funds::UnlockAndUnreserveAllFunds<UnlockConfig>,
-		pallet_tips::migrations::unreserve_deposits::UnreserveDeposits<UnlockConfig, ()>,
-		// Delete storage key/values from all Gov v1 pallets
-		frame_support::migrations::RemovePallet<CouncilPalletName, <Runtime as frame_system::Config>::DbWeight>,
-		frame_support::migrations::RemovePallet<
-			PhragmenElectionPalletName,
-			<Runtime as frame_system::Config>::DbWeight,
-		>,
-		frame_support::migrations::RemovePallet<TipsPalletName, <Runtime as frame_system::Config>::DbWeight>,
+		cumulus_pallet_xcmp_queue::migration::v5::MigrateV4ToV5<Runtime>,
 	);
 }
 
