4b08300e1a3aa08b09f14809598e35fb89315469
cl0w <v@lery.dev>
2026-04-13 18:25:46 +0200

    clippy

--- pallets/parameters/src/tests.rs
+++ pallets/parameters/src/tests.rs
@@ -2,14 +2,14 @@ use crate::{mock::*, Pallet as Parameters, RelayParentOffsetOverride};
 
 #[test]
 fn relay_parent_offset_override_is_false_by_default() {
-	ExtBuilder::default().build().execute_with(|| {
+	ExtBuilder.build().execute_with(|| {
 		assert!(!Parameters::<Test>::relay_parent_offset_override());
 	});
 }
 
 #[test]
 fn relay_parent_offset_override_can_be_read_from_storage() {
-	ExtBuilder::default().build().execute_with(|| {
+	ExtBuilder.build().execute_with(|| {
 		RelayParentOffsetOverride::<Test>::put(true);
 		assert!(Parameters::<Test>::relay_parent_offset_override());
 	});
