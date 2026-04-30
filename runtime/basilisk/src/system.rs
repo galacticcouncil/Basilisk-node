86a543d70eecd4e34f66ea6680826b18b7b1a136
cl0w <v@lery.dev>
2026-04-30 17:07:08 +0200

    pallet_timestampe minimumperiod 0

--- runtime/basilisk/src/system.rs
+++ runtime/basilisk/src/system.rs
@@ -242,7 +242,7 @@ impl pallet_timestamp::Config for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = u64;
 	type OnTimestampSet = ();
-	type MinimumPeriod = ConstU64<{ SLOT_DURATION / 2 }>;
+	type MinimumPeriod = ConstU64<0>;
 	type WeightInfo = weights::pallet_timestamp::BasiliskWeight<Runtime>;
 }
 
